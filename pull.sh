7d1d7bbcf035f929bd9c6f5695c72ed30a3c931c
Joe Barnett <joebarn@users.noreply.github.com>
2016-06-03 00:23:01 -0400

    $2

---
+++ pull.sh
@@ -0,0 +1,11 @@
+cd ../paralib-net
+echo
+echo '--------------------------------------------------'
+echo 'paralib-net:'
+git pull
+
+cd ../paralib-net-reference
+echo
+echo '--------------------------------------------------'
+echo 'paralib-net-refernce:'
+git pull
---
+++ status.sh
@@ -0,0 +1,11 @@
+cd ../paralib-net
+echo
+echo '--------------------------------------------------'
+echo 'paralib-net:'
+git status
+
+cd ../paralib-net-reference
+echo
+echo '--------------------------------------------------'
+echo 'paralib-net-refernce:'
+git status
