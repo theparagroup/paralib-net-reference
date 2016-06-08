8476115e3dd44a59ff8693d626f929860091f775
Joe Barnett <joebarn@users.noreply.github.com>
2016-06-08 13:56:16 -0400

    'logging'

--- paralib-reference-mvc/Controllers/HelloController.cs
+++ paralib-reference-mvc/Controllers/HelloController.cs
@@ -4,15 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using com.paralib.reference.mvc.Models;
+using log4net;
 
 namespace com.paralib.reference.mvc.Controllers
 {
-   public class HelloController : Controller
-   {
-      [Route("hello/index")]
-      public ActionResult Index()
-      {
-         return View(new Hello());
-      }
-   }
+    public class HelloController : Controller
+    {
+        private static ILog _logger = LogManager.GetLogger(typeof(HelloController));
+
+        [Route("hello/index")]
+        public ActionResult Index()
+        {
+            _logger.Info("hello world");
+            return View(new Hello());
+        }
+    }
 }
--- paralib-reference-mvc/Global.asax.cs
+++ paralib-reference-mvc/Global.asax.cs
@@ -1,51 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using com.paralib.common;
+using log4net;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace com.paralib.reference.mvc
 {
-   public class Global : System.Web.HttpApplication
-   {
+    public class Global : System.Web.HttpApplication
+    {
+        private static ILog _logger = LogManager.GetLogger(typeof(Global));
 
-      protected void Application_Start(object sender, EventArgs e)
-      {
-         RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-         RouteTable.Routes.MapMvcAttributeRoutes();
-      }
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            RouteTable.Routes.MapMvcAttributeRoutes();
 
-      protected void Session_Start(object sender, EventArgs e)
-      {
+            Configuration.Configure("mvc");
 
-      }
+            _logger.Info(null);
+        }
 
-      protected void Application_BeginRequest(object sender, EventArgs e)
-      {
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            _logger.Info(null);
+        }
 
-      }
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            _logger.Info(null);
+        }
 
-      protected void Application_AuthenticateRequest(object sender, EventArgs e)
-      {
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            _logger.Info(null);
+        }
 
-      }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            _logger.Fatal(ex.Message);
+        }
 
-      protected void Application_Error(object sender, EventArgs e)
-      {
+        protected void Session_End(object sender, EventArgs e)
+        {
+            _logger.Info(null);
+        }
 
-      }
-
-      protected void Session_End(object sender, EventArgs e)
-      {
-
-      }
-
-      protected void Application_End(object sender, EventArgs e)
-      {
-
-      }
-   }
+        protected void Application_End(object sender, EventArgs e)
+        {
+            _logger.Info(null);
+        }
+    }
 }
