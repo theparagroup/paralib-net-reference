79ba70dfc2b00423763dd4e1f2a9968bbd11e245
Joe Barnett <joebarn@users.noreply.github.com>
2016-06-20 18:52:19 -0400

    'logging finished'

--- paralib-reference-console/Program.cs
+++ paralib-reference-console/Program.cs
@@ -7,10 +7,13 @@ namespace paralib_reference_console
     {
         static void Main(string[] args)
         {
+            Paralib.Configure += Paralib_Configure;
 
             System.Threading.Thread.CurrentPrincipal = new System.Security.Principal.GenericPrincipal(new System.Security.Principal.GenericIdentity("joe"),new string[] { });
 
             Paralib.Initialize();
+            Paralib.RaiseConfigureEvent();
+            
 
             Console.WriteLine($"logging enabled = {Paralib.Configuration.Logging.Enabled}");
             Console.WriteLine($"logging debug = {Paralib.Configuration.Logging.Debug}");
@@ -28,6 +31,11 @@ namespace paralib_reference_console
             log.Error();
             log.Fatal();
 
+        }
+
+        private static void Paralib_Configure(com.paralib.Configuration.Settings settings)
+        {
+            settings.Logging.Logs.Add(new com.paralib.Logging.Log("joe", com.paralib.Logging.LogTypes.Database) { Table = "joe", Pattern = "%.100logger", Fields = "logger" });
         }
     }
 }
