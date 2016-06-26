eee6c70641b79c053400791f8e57d61d81990e62
Joe Barnett <joebarn@users.noreply.github.com>
2016-06-25 21:45:02 -0400

    'refactoring configuration'

--- paralib-reference-console/Program.cs
+++ paralib-reference-console/Program.cs
@@ -8,24 +8,6 @@ namespace com.paralib.reference.console
 
     class Employee
     {
-        //[StringLength(2)]
-        //[MinLength(20, ErrorMessage = "too small")]
-        //[MaxLength(2)]
-        public string FirstName { get; set; }
-
-        //[Required]
-        //[Display(Name = "Last Name")]
-        //[String(3)]
-        public string LastName { get; set; }
-
-        [Required]
-        [Display(Name = "Email Address")]
-        [ParaType(ParaTypes.Email)]
-        public string Email { get; set; }
-
-        [ParaType(ParaTypes.Zip4)]
-        public string Zip { get; set; }
-
 
     }
 
@@ -33,24 +15,6 @@ namespace com.paralib.reference.console
     {
         static void Main(string[] args)
         {
-            var e = new Employee();
-            e.Zip = "12345-1234";
-            e.Email = "no";
-
-            var errors = ObjectValidator.Validate(e);
-
-            if (errors!=null)
-            {
-                foreach (var r in errors)
-                {
-                    Console.WriteLine($"\t{r.MemberName} - {r.ErrorMessage} ");
-                }
-            }
-            else
-            {
-                Console.WriteLine("valid");
-            }
-
 
         }
 
