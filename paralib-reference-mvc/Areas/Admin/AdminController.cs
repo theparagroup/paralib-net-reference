4fb2a171dfc64c218b8423dbeaafd9b7a410f98f
Joe Barnett <joebarn@users.noreply.github.com>
2017-03-30 23:49:58 -0400

    'paralib refactoring'

--- paralib-reference-mvc/Areas/Admin/AdminController.cs
+++ paralib-reference-mvc/Areas/Admin/AdminController.cs
@@ -3,12 +3,13 @@ using System.Web.Mvc;
 using com.paralib.Mvc.Authorization;
 using com.paralib.Mvc.Authentication;
 using com.paralib.reference.mvc.Areas.Admin.Models;
+using com.paralib.Mvc.Infrastructure;
 
 namespace com.paralib.reference.mvc.Areas.Admin
 {
     [RouteArea("admin")]
     [Permissions(UnauthenticatedUrl = "~/admin/login", UnauthorizedUrl = "~/admin/error", Roles ="star,belly")]
-    public abstract class AdminController : Controller
+    public abstract class AdminController : ParaController
     {
 
     }
